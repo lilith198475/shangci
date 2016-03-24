2952c1126bd755e095d319b78e6a46ec3a8065bb
Will <Will_shengyi@hotmail.com>
2016-03-24 16:45:14 -0400

    Begin to do the catgory main page

---
+++ wp-content/themes/shangci/category-crafts.php
@@ -0,0 +1,79 @@
+<?php 
+/* 
+  Template Name: category - crafts
+*/
+?>
+
+<?php get_header(); ?>
+
+
+
+<!-- main content -->   
+
+  	 <div class="container category-container">
+     	<div class="row">
+      		<div class="col-md-10 col-md-offset-1  content-block">
+               <div  class="content-tag">
+               		<div class="row">
+                      <div class="col-xs-2 tab-active"><p><b>工艺分类</b></p></div>
+                      <div class="col-xs-2 tab-sleep"><a href="category-age.html"><P><b>年代分类</b></P></a></div>
+                      <div class="col-xs-2 tab-sleep"><a href="category-Kilns.html"><p><b>窑口分类</b></p></a></div>
+                      <div class="col-xs-2 tab-sleep"><a href="category-shape.html"><p><b>器型分类</b></p></a></div>
+                      <div class="col-xs-2 tab-sleep"><a href="category-glaze.html"><p><b>釉色分类</b></p></a></div>    
+                      <div class="col-xs-2 tab-sleep"><a href="category-people.html"><p><b>名家名瓷</b></p></a></div>
+                    </div>
+               </div>  
+               
+              <div class="content-main">
+                 <div class="row items-row"> 
+                 	<div class="col-md-3">
+                        <a href="#"><div class="thumbnail" ><figure class="tint"><img src="img/thumbnail/1.jpg" alt="" class="img-responsive img-circle"  ></figure></div>
+                        <h5><b>将军罐</b></h5></a>
+                        
+            		</div>
+            
+                    <div class="col-md-3">
+                        <a href="#"><div class="thumbnail" ><figure class="tint"><img src="img/thumbnail/2.jpg" alt="" class="img-responsive img-circle" ></figure></div>
+                        <h5><b>双系耳罐</b></h5></a>
+                    </div>
+            
+                    <div class="col-md-3">
+                        <a href="#"><div class="thumbnail" ><figure class="tint"><img src="img/thumbnail/3.jpg" alt="" class="img-responsive img-circle" ></figure></div>
+                        <h5><b>观音瓶</b></h5></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#"><div class="thumbnail" ><figure class="tint"><img src="img/thumbnail/4.jpg" alt="" class="img-responsive img-circle"  ></figure></div>
+                        <h5><b>棒槌瓶</b></h5></a>
+                    </div> 
+                    </div>
+                    <div class="row items-row"> 
+                    
+                    
+                    	<div class="col-md-3">
+                        <a href="#"><div class="thumbnail" ><figure class="tint"><img src="img/thumbnail/1.jpg" alt="" class="img-responsive img-circle"  ></figure></div>
+                        <h5><b>将军罐</b></h5></a>
+                        
+            		</div>
+            
+                    <div class="col-md-3">
+                        <a href="#"><div class="thumbnail" ><figure class="tint"><img src="img/thumbnail/2.jpg" alt="" class="img-responsive img-circle" ></figure></div>
+                        <h5><b>双系耳罐</b></h5></a>
+                    </div>
+            
+                    <div class="col-md-3">
+                        <a href="#"><div class="thumbnail" ><figure class="tint"><img src="img/thumbnail/3.jpg" alt="" class="img-responsive img-circle" ></figure></div>
+                        <h5><b>观音瓶</b></h5></a>
+                    </div>
+                    <div class="col-md-3">
+                        <a href="#"><div class="thumbnail" ><figure class="tint"><img src="img/thumbnail/4.jpg" alt="" class="img-responsive img-circle"  ></figure></div>
+                        <h5><b>棒槌瓶</b></h5></a>
+                    </div> 
+                 </div>
+              </div>
+ 		   </div>
+		</div>
+	</div>
+
+
+
+<?php get_footer() ?>
