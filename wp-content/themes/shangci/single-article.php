64e3c89fe3df55babfbdcd95b73eec5f0dda202d
Will <Will_shengyi@hotmail.com>
2016-03-30 09:27:00 -0400

    test

---
+++ wp-content/themes/shangci/single-article.php
@@ -0,0 +1,133 @@
+<?php get_header(); ?>
+  
+<!-- main content -->   
+ 
+     <div class="container">   
+        <div class="row ">
+        	<div class="col-sm-10 col-sm-offset-1">
+              <div class="row">
+            
+                    <div class="single-report-head-image">
+                      <img class="img-responsive" src="../img/single-report/1/1.jpg">
+                    </div>
+                 </div>  
+                 
+                 <div class="row">
+                    <div class="col-xs-10 col-xs-offset-1">
+                    
+                    <h5 class="text-center"><span>新闻 | 专题</span></h5>
+                                
+                    </div>
+           		</div> 
+                
+                <div class="row">
+           		
+                 	<div class="row text-center">
+                     <h1>清十八世紀 青釉盤</h1>
+                     <p>直徑 11.1 公分</p>
+                    
+                    </div>  
+          	   </div>
+           
+              
+                <div class="row item-content report">
+                  <div class="col-sm-10 col-sm-offset-1">
+                 
+                   <p>青釉包括豆青、东青、粉青、仿龙泉釉。其中豆青为上品。青釉成熟于康熙，经过雍正的发展，在乾隆年间达到精致，应该说这一时期的青釉瓷的稍早技术已经达到了历史最高峰！青釉均用紫金釉微掺青料合成。许之衡《饮流斋说瓷》称：“古瓷尚青，凡绿也、蓝也，皆以青括之。”刘子芬《竹园陶说》中也说：“青色一种，常与蓝色相混。雨过天晴，钧窑、元窑之青，皆近蓝色。“惟千峰翠色、梅子青、豆青、乃为纯青耳。天色本蓝，有时为青。”从传世品看，青釉康熙时为多，乾隆次之，雍正较少。青釉特点为釉色古雅、沉稳，釉面均匀、滋润，釉质坚致、细腻！</p>
+                   
+                   <img src="../img/single-report/1/2.jpg">
+                   
+                   <p>青釉包括豆青、东青、粉青、仿龙泉釉。其中豆青为上品。青釉成熟于康熙，经过雍正的发展，在乾隆年间达到精致，应该说这一时期的青釉瓷的稍早技术已经达到了历史最高峰！青釉均用紫金釉微掺青料合成。许之衡《饮流斋说瓷》称：“古瓷尚青，凡绿也、蓝也，皆以青括之。”刘子芬《竹园陶说》中也说：“青色一种，常与蓝色相混。雨过天晴，钧窑、元窑之青，皆近蓝色。“惟千峰翠色、梅子青、豆青、乃为纯青耳。天色本蓝，有时为青。”从传世品看，青釉康熙时为多，乾隆次之，雍正较少。青釉特点为釉色古雅、沉稳，釉面均匀、滋润，釉质坚致、细腻！</p>
+                   
+                   <img src="../img/single-report/1/3.jpg">
+                   
+                   <p>青釉包括豆青、东青、粉青、仿龙泉釉。其中豆青为上品。青釉成熟于康熙，经过雍正的发展，在乾隆年间达到精致，应该说这一时期的青釉瓷的稍早技术已经达到了历史最高峰！青釉均用紫金釉微掺青料合成。许之衡《饮流斋说瓷》称：“古瓷尚青，凡绿也、蓝也，皆以青括之。”刘子芬《竹园陶说》中也说：“青色一种，常与蓝色相混。雨过天晴，钧窑、元窑之青，皆近蓝色。“惟千峰翠色、梅子青、豆青、乃为纯青耳。天色本蓝，有时为青。”从传世品看，青釉康熙时为多，乾隆次之，雍正较少。青釉特点为釉色古雅、沉稳，釉面均匀、滋润，釉质坚致、细腻！</p>
+             	  </div>
+                </div>
+                
+                 <div class="row item-content">
+                    <div class="col-xs-4 col-xs-offset-4">
+                    	<div class="row">
+               			<div class="col-xs-3 social">
+                          <a href="#" class="social-icon">
+                            <i class="fa fa-facebook"></i>
+                            
+                           </a> 
+                          </div>
+                          <div class="col-xs-3 social">
+                          <a href="#" class="social-icon">
+                            <i class="fa fa-weixin"></i>
+                       
+                            </a>
+                          </div>
+                          <div class="col-xs-3 social">
+                          <a href="#" class="social-icon">
+                            <i class="fa fa-instagram"></i>
+                      
+                            </a>
+                          </div>
+                          <div class="col-xs-3 social">
+                          <a href="#" class="social-icon">
+                            <i class="fa fa-weibo"></i>
+                      
+                            </a>
+                          </div>
+                  </div> 
+                  </div>
+                  </div>
+           
+                <hr>
+                
+                
+               <div class="row">
+                
+                            <h4><b><a href="../blog.html">最新专题</a></b></h4>
+                         <div class="col-md-3">
+                            <a href="../single-report/1.html"><div class="thumbnail" ><figure class="tint"><img src="../img/single-report/1/1.jpg" alt="" class="img-responsive img-rounded"  ></figure></div>
+                            <h5><b>中國瓷器收藏指南：七大注意事項 </b></h5>
+                          
+                        </div>
+                        
+                         <div class="col-md-3">
+                            <a href="../single-report/1.html"><div class="thumbnail" ><figure class="tint"><img src="../img/single-report/2/1.jpg" alt="" class="img-responsive img-rounded"  ></figure></div>
+                            <h5><b>2015年秋季中國瓷器及工藝精品拍賣精選 </b></h5>
+                          
+                        </div>
+                        
+                         <div class="col-md-3">
+                            <a href="../single-report/1.html"><div class="thumbnail" ><figure class="tint"><img src="../img/single-report/3/1.jpg" alt="" class="img-responsive img-rounded"  ></figure></div>
+                            <h5><b>宋瓷收藏指南 </b></h5>
+                          
+                        </div>
+                        
+                         <div class="col-md-3">
+                            <a href="../single-report/1.html"><div class="thumbnail" ><figure class="tint"><img src="../img/single-report/1/1.jpg" alt="" class="img-responsive img-rounded"  ></figure></div>
+                            <h5><b>中國瓷器收藏指南：七大注意事項 </b></h5>
+                          
+                        </div>
+                        
+                         <h5  class="pull-right"><b><a href="../reports.html">更多专题...</a></b></h5>
+                         
+   				</div>
+            </div>  
+        </div>
+      </div>
+      
+     
+ 
+ <!-- Light Box -->  
+ <div id="myModal" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+  <div class="modal-content">
+	<div class="modal-body">
+    <button type="button" class="close" data-dismiss="modal">×</button>
+		<img src="" class="img-responsive">
+	</div>
+	<div class="modal-footer">
+		<button class="btn btn-default" data-dismiss="modal">Close</button>
+	</div>
+   </div>
+  </div>
+</div>
+ 
+<?php get_footer(); ?>
